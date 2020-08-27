5825b9fb698f4c4ecc26dc1877e9adeb87cc3b1c
Volodymyr Badai <vbadai@callfire.com>
2020-08-27 12:20:22 +0300

    improvements, shell

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # test-bookinfo
 bookinfo test app
+
+```
+source env.properties # load variables
+export PATH=$PATH:${PWD}/istio-${ISTIO_VERSION}/bin # add istio to PATH
+istioctl dash grafana # start grafana dashboard
+```
+
--- start.sh
+++ start.sh
@@ -29,15 +29,36 @@ start_istio() {
   echo "istioctl version:"
   istioctl version
   echo "istioctl install:"
+  # generating demo profile manifest to check setup after
   istioctl manifest generate --set profile=demo > generated-manifest.yaml
-  istioctl verify-install -f generated-manifest.yaml || ( istioctl install --set profile=demo && istioctl verify-install -f generated-manifest.yaml )
+  istioctl verify-install -f generated-manifest.yaml 2>/dev/null || \
+    (istioctl install --set profile=demo && istioctl verify-install -f generated-manifest.yaml 2>&1)
+  # enable to inject sidecar proxies in pods
+  kubectl label namespace default istio-injection=enabled
 }
 
 start_minikube() {
   echo "minikube version:"
   minikube version
   echo "minikube start cluster:"
-  minikube status  || ( minikube start --driver=$MINIKUBE_DRIVER && minikube status)
+  minikube status || \
+    (minikube start --driver=$MINIKUBE_DRIVER --cpus $MINIKUBE_CPU --memory $MINIKUBE_RAM --addons ingress --addons metrics-server \
+    && minikube status)
+  echo "kubectl current-context:"
+  minikube kubectl config current-context
+}
+
+start_python_shell() {
+  python3 --version && pip --version || (
+    echo >&2 "please check that you have 'python3' and 'pip' installed "
+    exit 1
+  )
+  echo "install pipenv:"
+  pipenv --version || pip install --user pipenv
+  echo "install local env:"
+  pipenv install
+  echo "loading local env:"
+  pipenv shell
 }
 
 #start environment
@@ -50,6 +71,7 @@ main() {
   load_env_properties
   check_env
   start_env
+  start_python_shell
 }
 
 main "$@"
