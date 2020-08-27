dd1d7d83d6b53ad8c9766b5a34f9b2ed1ffb833b
Volodymyr Badai <vbadai@callfire.com>
2020-08-27 16:51:32 +0300

    fix

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def main():
     app = App("manifests/bookinfo.yaml")
     network = App("manifests/bookinfo-gateway.yaml")
 
-    # app.k_apply()
-    # network.k_apply()
+    app.k_apply()
+    network.k_apply()
 
     # app.k_check_status()
 
     # uncomment to delete resources
-    app.k_delete()
-    network.k_delete()
+    # app.k_delete()
+    # network.k_delete()
 
 
 if __name__ == '__main__':
